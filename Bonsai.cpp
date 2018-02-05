fcfbaffa24255baf28f1fac4205f946bf1bb3d86
CZYK <danko@czyk.nl>
2018-02-05 23:21:25 +0100

    Updated sourcecode

--- Bonsai.cpp
+++ Bonsai.cpp
@@ -18,7 +18,7 @@ Bonsai::Bonsai(String name, int pump_pin, int sensor_power_pin, int sensor_pin, 
   _sensor_pin = sensor_pin;
   _desired_moisture_level = desired_moisture;
   _watering_duration_ms = watering_duration_ms;
-  _interval = ONEHOUR;
+  _interval = 30UL * ONEMINUTE;
 
   _runs = 0;
 
